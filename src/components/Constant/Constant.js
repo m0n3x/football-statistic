bdd417f82af8f281828a33c0a9780a3072f5a64c
Konstantin Semenenko <m0n3x@yandex.by>
2021-08-04 20:10:55 +0300

    move leagues arr into file with constants

--- src/components/Constant/Constant.js
+++ src/components/Constant/Constant.js
@@ -1,3 +1,10 @@
 export const FETCHURL = "https://api.football-data.org/v2/";
 export const TOKEN = { "X-Auth-Token": "90781ba256f140ebbce7d262f211d222" }
 
+export const LEAGUES = [
+  { name: "English Premier League", id: 2021 },
+  { name: "German 1. Bundesliga", id: 2002 },
+  { name: "Spanish Primera", id: 2014 },
+  { name: "Italian Serie A", id: 2019 },
+  { name: "French League 1", id: 2015 },
+];
